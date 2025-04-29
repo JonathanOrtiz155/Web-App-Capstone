a9ff0d246e6708181dc14515b4f0fcab81c65c60
JonathanOrtiz155 <JonOrtiz1202@gmail.com>
2025-04-29 16:13:36 -0700

    this better workk

--- agent.js
+++ agent.js
@@ -2,7 +2,7 @@
 import fetch from "node-fetch";
 
 const PC_ID = "N-6-20437-20A";
-const BASE = "https://26be-2603-8000-cf01-26cc-9d64-3946-58ca-797.ngrok-free.app";
+const BASE = "https://747c-2603-8000-cf01-26cc-6c53-601e-4105-97de.ngrok-free.app";
 const BACKEND_URL = `${BASE}/api/heartbeat`;
 
 async function sendHeartbeat() {
