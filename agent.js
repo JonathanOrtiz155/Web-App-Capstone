417e4bcc764639c80e33521fc03b3bfcd4b3d8a0
Ortiz <jonathan.ortiz155@csuci.edu>
2025-05-02 13:21:09 -0700

    url change + sidebar home

--- agent.js
+++ agent.js
@@ -2,7 +2,7 @@
 import fetch from "node-fetch";
 
 const PC_ID = "N-6-20437-20A";
-const BASE = "https://6ffa-209-129-115-57.ngrok-free.app";
+const BASE = "https://522b-209-129-115-57.ngrok-free.app";
 const BACKEND_URL = `${BASE}/api/heartbeat`;
 
 async function sendHeartbeat() {
